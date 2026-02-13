7b312ef415c6e0cb30e79699087a0c6121546588
sumeshhesums <sumeshsumeshjkimo625@gmail.com>
2026-02-13 12:12:35 +0530

    Refactor blog views for improved query efficiency

--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -1,73 +1,100 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
+from django.db.models import Prefetch
 from .models import Post, Category, Comment
 from .forms import CommentForm
 
 
 def home(request):
-    """Home page view - TEAM MEMBER 3 RESPONSIBILITY"""
-    latest_posts = Post.objects.filter(published=True)[:5]
+    """Home page view"""
+
+    latest_posts = (
+        Post.objects
+        .filter(status='published')
+        .select_related('author', 'category')
+        .order_by('-created_date')[:5]
+    )
+
     categories = Category.objects.all()
-    
-    context = {
+
+    return render(request, 'blog/home.html', {
         'latest_posts': latest_posts,
         'categories': categories,
-    }
-    return render(request, 'blog/home.html', context)
+    })
 
 
 def post_list(request):
-    """List all blog posts - TEAM MEMBER 3 RESPONSIBILITY"""
-    posts = Post.objects.filter(published=True)
+    """List all blog posts"""
+
+    posts = (
+        Post.objects
+        .filter(status='published')
+        .select_related('author', 'category')
+        .order_by('-created_date')
+    )
+
     categories = Category.objects.all()
-    
+
     category_filter = request.GET.get('category')
+
     if category_filter:
         posts = posts.filter(category__name=category_filter)
-    
-    context = {
+
+    return render(request, 'blog/post_list.html', {
         'posts': posts,
         'categories': categories,
         'selected_category': category_filter,
-    }
-    return render(request, 'blog/post_list.html', context)
+    })
 
 
-def post_detail(request, pk):
-    """Single post detail view - TEAM MEMBER 3 RESPONSIBILITY"""
-    post = get_object_or_404(Post, pk=pk)
+def post_detail(request, slug):
+    """Single post detail view"""
+
+    post = get_object_or_404(
+        Post.objects.select_related('author', 'category'),
+        slug=slug,
+        status='published'
+    )
+
     comments = post.comments.filter(approved=True)
-    
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
+
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
+
+            # If comments require login
+            if request.user.is_authenticated:
+                comment.author = request.user
+
             comment.save()
             messages.success(request, 'Comment added successfully!')
-            return render(request, 'blog/post_detail.html', {
-                'post': post,
-                'comments': comments,
-                'form': CommentForm(),
-            })
+            return redirect('blog:post_detail', slug=post.slug)
     else:
         form = CommentForm()
-    
-    context = {
+
+    return render(request, 'blog/post_detail.html', {
         'post': post,
         'comments': comments,
         'form': form,
-    }
-    return render(request, 'blog/post_detail.html', context)
+    })
 
 
 def category_posts(request, category_name):
-    """Posts by category - TEAM MEMBER 3 RESPONSIBILITY"""
+    """Posts by category"""
+
     category = get_object_or_404(Category, name=category_name)
-    posts = Post.objects.filter(category=category, published=True)
-    
-    context = {
+
+    posts = (
+        Post.objects
+        .filter(category=category, status='published')
+        .select_related('author')
+        .order_by('-created_date')
+    )
+
+    return render(request, 'blog/category_posts.html', {
         'category': category,
         'posts': posts,
-    }
-    return render(request, 'blog/category_posts.html', context)
+    })
