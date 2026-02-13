5918a4fd83bf3c6bcb8af29639125f95653cdbeb
Gnanasree29 <gnanasreebgnana@gmail.com>
2026-02-13 12:17:41 +0530

    Refactor manage.py to include admin models

--- simple_blog/manage.py
+++ simple_blog/manage.py
@@ -1,22 +1,58 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from django.contrib import admin
+from .models import Category, Post, Comment
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_blog.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+class CommentInline(admin.TabularInline):
+    """Inline comments for posts."""
+    model = Comment
+    extra = 0
+    fields = ['author', 'content', 'approved', 'created_date']
+    readonly_fields = ['created_date']
+    ordering = ['-created_date']
 
 
-if __name__ == '__main__':
-    main()
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin - TEAM MEMBER 2 RESPONSIBILITY"""
+    list_display = ['name', 'created_date', 'post_count']
+    search_fields = ['name']
+    ordering = ['name']
+    
+    def post_count(self, obj):
+        """Display number of posts in category."""
+        return obj.post_set.count()
+    post_count.short_description = 'Posts'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin - TEAM MEMBER 2 RESPONSIBILITY"""
+    list_display = ['title', 'author', 'category', 'published', 'created_date', 'comment_count']
+    list_filter = ['published', 'created_date', 'category']
+    search_fields = ['title', 'content', 'author__username']
+    list_editable = ['published']
+    date_hierarchy = 'created_date'
+    ordering = ['-created_date']
+    inlines = [CommentInline]
+    
+    def comment_count(self, obj):
+        """Display number of comments on post."""
+        return obj.comment_set.count()
+    comment_count.short_description = 'Comments'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Comment admin - TEAM MEMBER 3 RESPONSIBILITY"""
+    list_display = ['author', 'post_link', 'approved', 'created_date']
+    list_filter = ['approved', 'created_date']
+    search_fields = ['author', 'content']
+    list_editable = ['approved']
+    readonly_fields = ['created_date']
+    ordering = ['-created_date']
+    
+    def post_link(self, obj):
+        """Link to parent post."""
+        return obj.post.title
+    post_link.short_description = 'Post'
+    post_link.admin_order_field = 'post__title'
